3f85f7d8c465f2f69437b022d016591e10e4b126
zhouxin312 <zhouxin312@qq.com>
2021-10-03 21:48:50 +0800

    temp

--- cricetinae/main.sh
+++ cricetinae/main.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-#dependence sed mp3info iconv
+#dependence ffprobe iconv
 SOURCE_DIR=~/transfer
 TARGET_DIR=~/cricetinae
 SUB_CATEGORY=(music)
 
 declare -A RELATION_LIST
-RELATION_LIST=
+RELATION_LIST=\
 (
 	["mp3"]=mp3_handle
 	["gba"]=generate_handle
 )
 source ./music.sh
-#接受目标文件夹的绝对路径
 function literate()
 {
-	for file in $(ls $1)
+	#在这里，如果$1是相对路径，需要将其转换为绝对路径
+	for file in "$(ls $1)"
 	do
-		file=$1"/"$file
-		if [ -d $file ]
+		#在这里需要解决，如果文件或目录名的编码是gb2312，则将其转换为UTF-8
+		file=$1"/""$file"
+		if [ -d "$file" ]
 		then
-			literate $file
+			literate "$file"
 		else
-			for suffix in $(cut -d , -f 1 $RELATION_LIST)
-			do
-				if [ ${file##*.} = $suffix ]
-				then
-					local func=$(sed -n /$suffix/p $RELATION_LIST|cut -d, -f2)
-					$func $file
-				fi
-			done
+			if ! [ -z ${RELATION_LIST["${file##*.}"]} ]
+			then
+				local func=${RELATION_LIST["${file##*.}"]}
+				$func "$file"
+			fi
 		fi
 	done
 }
--- cricetinae/music.sh
+++ cricetinae/music.sh
@@ -1,11 +1,12 @@
 function mp3_handle()
 {
-	local artist=`mp3info -p %a "$1"|iconv -f gb2312 -t UTF-8`
-	local sor=$1
+	local artist=`ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 "$1"`
+	#提取艺术家名字是否有更好的方法，需解决ID3V1和ID3V2不同的问题，还有gb2312到UTF-8转换的问题
+	local sor="$1"
 	local target=$TARGET_DIR/${SUB_CATEGORY[0]}/$artist
 	if ! [ -d $target ]
 	then
 		mkdir -p $target
 	fi
-	mv $sor $target
+	mv "$sor" $target
 }
