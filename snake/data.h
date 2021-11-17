4ba92f8fe8e77a1a78437c79e8ea5deab0a528be
zhouxin <zhouxin312@qq.com>
2021-11-17 14:06:43 +0800

    2021年 11月 17日 星期三 14:06:43 CSTata home

--- snake/data.h
+++ snake/data.h
@@ -1,2 +1,20 @@
-#include <ndbm.h>
+#ifndef _DATA_H
+#define _DATA_H
 
+#include <ndbm.h>
+#define WINDOW_WIDTH 40
+#define WINDOW_HEIGHT 20
+#define TOTLE_POINT (18*38)
+typedef struct
+{
+	int x;
+	int y;
+}node;
+typedef node food;
+typedef node *snake;
+
+
+
+
+
+#endif
---
+++ snake/main.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <curses.h>
+
+#include "data.h"
+
+int Current_len=2;
+bool Map[WINDOW_HEIGHT-2][WINDOW_WIDTH-2]=(false);
+int main(int argc,char *argv[])
+{
+	int command_result;
+	if(argc>1)
+	{
+		command_result=command_mode(argc,argv);
+		if(command_result)
+			exit(command_result);
+	}
+}
